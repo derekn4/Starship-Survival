f905f0e20c0abfc198ac804192dfd5ff9b482ab4
Derek Nguyen <32850977+derekn4@users.noreply.github.com>
2020-03-11 17:19:56 -0700

    Add files via upload

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float maxVelocity = 3;
+
+    public float rotationSpeed = 3;
+
+    #region Monobehaviour API
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float yAxis = Input.GetAxis("Vertical");
+        float xAxis = Input.GetAxis("Horizontal");
+
+        ThrustFoward(yAxis);
+        Rotate(transform, xAxis * -rotationSpeed);
+    }
+    #endregion
+
+    #region Maneuvering API
+
+    private void ClampVelocity()
+    {
+        float x = Mathf.Clamp(rb.velocity.x, -maxVelocity, maxVelocity);
+        float y = Mathf.Clamp(rb.velocity.y, -maxVelocity, maxVelocity);
+
+        rb.velocity = new Vector2(x, y);
+    }
+
+    private void ThrustFoward(float amount)
+    {
+        Vector2 force = transform.up * amount;
+
+        rb.AddForce(force);
+    }
+
+    private void Rotate(Transform t, float amount)
+    {
+        t.Rotate(0, 0, amount);
+    }
+    #endregion
+}
